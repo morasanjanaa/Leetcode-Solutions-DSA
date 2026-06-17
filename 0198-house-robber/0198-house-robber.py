fc5f13f37b4ad4f1a9fce168f4dbcbe528b46f26
morasanjanaa <morasanjanaa@gmail.com>
2026-06-17 23:12:36 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.44%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def solve(self,nums,i,dp):
+        if i >= len(nums):
+            return 0
+        if dp[i] != -1:
+            return dp[i]
+        steal = nums[i] + self.solve(nums,i+2,dp)
+        skip = self.solve(nums,i+1,dp)
+        dp[i] = max(steal,skip)
+        return dp[i]
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        return self.solve(nums,0,dp)
+    
+
+
+        

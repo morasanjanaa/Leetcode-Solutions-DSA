83908a58e658591a5ea2a54de8484dcd1cc6c7d1
morasanjanaa <morasanjanaa@gmail.com>
2026-06-18 17:52:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,20 @@
 class Solution:  
     def rob(self, nums: List[int]) -> int:
-        # Bottom Up Solution
+        #Constant space
         n = len(nums)
-        dp = [-1] * (n+1)
-        dp[0] = 0
-        dp[1] = nums[0]
+        prevprev = 0
+        prev = nums[0]
         for i in range(2,n+1):
             if i-2 > 0:
-                steal = nums[i-1] + dp[i-2]
+                steal = nums[i-1] + prevprev
             else:
                 steal = nums[i-1]
-            skip = dp[i-1]
-            dp[i] = max(skip,steal)
-        return dp[n]
+            skip = prev
+            temp = max(skip,steal)
+            prevprev = prev
+            prev = temp
+        return prev
 
-
-    
-
+        
 
         

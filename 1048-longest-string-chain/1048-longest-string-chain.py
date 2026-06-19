a0f2ea99152cd90611fea059afe6acaad480a3e0
morasanjanaa <morasanjanaa@gmail.com>
2026-06-19 16:10:55 +0530

    Time: 843 ms (13.1%), Space: 19.5 MB (69.15%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -16,6 +16,38 @@ class Solution:
                 j+=1
 
             return i == prev_len
+        # Bottom Up solution
+        words.sort(key=len)
+        n = len(words)
+        dp = [1]*n
+        maxi = 1
+        for i in range(n):
+            for j in range(i):
+                if isPred(words[i],words[j]):
+                    dp[i] = max(dp[i],dp[j]+1)
+                    maxi = max(maxi,dp[i])
+        return maxi
+
+ 
+
+
+        '''
+        ----Recursion + memorization
+        def isPred(cur,prev):
+            cur_len = len(cur)
+            prev_len = len(prev)
+
+            if prev_len + 1 != cur_len:
+                return False
+
+            i,j = 0,0
+             
+            while(i<prev_len and j<cur_len):
+                if cur[j] == prev[i]:
+                    i+=1
+                j+=1
+
+            return i == prev_len
 
         @lru_cache(None)
         def solve(cur_idx,prev_idx):
@@ -34,10 +66,11 @@ class Solution:
 
             if prev_idx != -1:
                 dp[cur_idx][prev_idx] = max(skip,take)
-                
+
             return max(skip,take)
 
         words.sort(key=len)
         n = len(words)
         dp = [[-1] * (n+1) for _ in range(n+1)]
         return solve(0,-1)
+        '''

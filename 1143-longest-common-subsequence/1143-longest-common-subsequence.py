c39ffecab0df5b6997d5e4cb9446f6ce97076490
morasanjanaa <morasanjanaa@gmail.com>
2026-06-20 19:20:27 +0530

    Time: 351 ms (67.89%), Space: 44.3 MB (73.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,6 +1,19 @@
 from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        #Botom up
+        m = len(s1)
+        n = len(s2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[m][n]
+            
+        '''
         @lru_cache(None)
         def solve(i,j):
             if i >= m or j >= n:
@@ -11,5 +24,6 @@ class Solution:
         m = len(s1)
         n = len(s2)
         return solve(0,0)
+        '''
 
         

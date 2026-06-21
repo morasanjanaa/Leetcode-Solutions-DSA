b479c1a73df7d6e5166fbf74373e2d77d680cb79
morasanjanaa <morasanjanaa@gmail.com>
2026-06-21 19:39:57 +0530

    Time: 166 ms (72.03%), Space: 19.4 MB (53.44%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,8 +1,37 @@
-from functools import lru_cache
 from typing import List
 
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        nums.sort()
+        n = len(nums)
+
+        dp = [1] * n          # Length of largest divisible subset ending at i
+        parent = [-1] * n     # To reconstruct the subset
+
+        max_len = 1
+        last_index = 0
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    parent[i] = j
+
+            if dp[i] > max_len:
+                max_len = dp[i]
+                last_index = i
+
+        # Reconstruct the subset
+        ans = []
+        while last_index != -1:
+            ans.append(nums[last_index])
+            last_index = parent[last_index]
+
+        return ans[::-1]
+        '''
         nums.sort()
         n = len(nums)
 
@@ -24,3 +53,4 @@ class Solution:
             return skip
 
         return list(solve(0, -1))
+        '''

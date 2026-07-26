ebf587b7e51bd9143081641194a3ac6b21fc71aa
morasanjanaa <morasanjanaa@gmail.com>
2026-07-26 21:28:17 +0530

    Time: 87 ms (10.6%), Space: 44 MB (17.14%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,36 +1,59 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-
-        neg = []
-        pos = []
-        ans = []
+        neg_arr = []
+        pos_arr = []
+        
         for i in nums:
-            if i > 0:
-                pos.append(i)
+            if i < 0:
+                neg_arr.append(i)
             else:
-                neg.append(i)
-
+                pos_arr.append(i)
         i = 0
-        j = 0
-        temp = 0
-
-        while(temp<len(nums)):
-            if temp%2==0:
-                ans.append(pos[i])
-                i += 1
+        pos = 0
+        neg = 0
+        arr = [-1]*len(nums)
+        
+        while(i < len(nums)):
+            if i%2 == 0:
+                arr[i] = pos_arr[pos]
+                pos += 1
             else:
-                ans.append(neg[j])
-                j += 1
-            temp += 1
+                arr[i] = neg_arr[neg]
+                neg += 1
+            i += 1
+       
+        return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        pos_index=0
+        neg_index=1
+
+        ans=[0]*len(nums)
+
+        for i in range(len(nums)):
+            if nums[i]<0:
+                ans[neg_index]=nums[i]
+                neg_index+=2
+            else:
+                ans[pos_index]=nums[i]
+                pos_index+=2
 
         return ans
-    
-            
-
-       
-
-
-
-
-
-        
+        '''

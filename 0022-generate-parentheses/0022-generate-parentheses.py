f03eaf0a467cb226a2b080b320b4495051030b46
morasanjanaa <morasanjanaa@gmail.com>
2026-07-26 16:31:21 +0530

    Time: 3 ms (29.74%), Space: 21 MB (9.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,6 +1,8 @@
+from functools import lru_cache
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
+        @lru_cache(None)
         def generate(left,right,s):
             if right == n:
                 res.append(s)

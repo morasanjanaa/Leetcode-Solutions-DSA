10dbacb7f228eaa7035b57c1a34d14aac033dccd
morasanjanaa <morasanjanaa@gmail.com>
2026-07-27 10:13:03 +0530

    Time: 3 ms (60.76%), Space: 21 MB (69.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        missing_num = 0
+        for num in nums:
+            missing_num ^=num
+        return missing_num
+
+        

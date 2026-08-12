6caf36ab3404acb27f1ed9c5beb0d54628a6229a
morasanjanaa <morasanjanaa@gmail.com>
2026-08-12 20:49:37 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.95%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,6 +5,50 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # Calculate the size for of LL
+
+        size = 0
+        temp = head
+        while temp:
+            size += 1
+            temp = temp.next
+
+        slow = head
+        fast = head
+
+        if size % 2 == 0:
+            while(fast):
+                slow = slow.next
+                fast = fast.next.next
+        else:
+            while(fast.next):
+                slow = slow.next
+                fast = fast.next.next
+                
+        return slow
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
         temp = head
         size = 0
 
@@ -21,7 +65,7 @@ class Solution:
             mid -= 1
 
         return temp
-
+      '''
 
         
 
